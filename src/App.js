cba40aa699143c9068e5d9a1591d089378adedbe
zadigg <abelkibebe5@gmail.com>
2023-11-19 13:30:33 -0600

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import * as Realm from "realm-web";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const app = new Realm.App({ id: "application-0-uiref" });
+
+const App = () => {
+    const [user, setUser] = useState();
+    const [activeDevelopers, setActiveDevelopers] = useState([]);
+
+    useEffect(() => {
+        const login = async () => {
+            try {
+                const user = await app.logIn(Realm.Credentials.anonymous());
+                setUser(user);
+
+                const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+                const collection = mongodb.db("Score-estimation-core").collection("teams");
+
+                // Watch for changes in the 'teams' collection
+                for await (const change of collection.watch()) {
+                    const activeDev = change.fullDocument?.developers.filter(
+                        (developer) => developer.status === "ACTIVE"
+                    );
+                    setActiveDevelopers(activeDev || []);
+                }
+            } catch (error) {
+                console.error("Error:", error);
+            }
+        };
+        login();
+    }, []);
+
+    return (
+        <div className="App">
+            {!!user && (
+                <div className="App-header">
+                    <h1>Connected as user {user.id}</h1>
+                    <div>
+                        <p>Active Developers:</p>
+                        <ul>
+                            {activeDevelopers.map((developer, index) => (
+                                <li key={index}>{developer.name}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default App;
