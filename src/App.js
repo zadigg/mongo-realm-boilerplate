ec965c2992b58d2455fd0990dce6a3b24d798107
zadigg <abelkibebe5@gmail.com>
2023-11-19 13:44:58 -0600

    🔧 fix(App.js): fix import statements formatting and indentation for better readability ✨ feat(App.js): add state variable 'events' to store latest events from the database 🔧 fix(App.js): fix formatting and indentation of useEffect hook for better readability ✨ feat(App.js): add table to display latest events from the 'teams' collection in the database

--- src/App.js
+++ src/App.js
@@ -1,52 +1,61 @@
-import React, { useState, useEffect } from "react";
-import * as Realm from "realm-web";
 
-const app = new Realm.App({ id: "application-0-uiref" });
+import  React, {useState, useEffect} from  "react"
+import  *  as  Realm  from  "realm-web";
 
-const App = () => {
+const  app = new  Realm.App({ id:  "application-0-uiref"});
+
+// Define the App component
+
+const  App = () => {
+  // Set state variables
     const [user, setUser] = useState();
-    const [activeDevelopers, setActiveDevelopers] = useState([]);
+    const [events, setEvents] = useState([]);
+
+
+  // This useEffect hook will run only once when the page is loaded
 
     useEffect(() => {
-        const login = async () => {
-            try {
-                const user = await app.logIn(Realm.Credentials.anonymous());
-                setUser(user);
+        const  login = async () => {
+            // Authenticate anonymously
+            const  user = await  app.logIn(Realm.Credentials.anonymous());
+            setUser(user);
 
-                const mongodb = app.currentUser.mongoClient("mongodb-atlas");
-                const collection = mongodb.db("Score-estimation-core").collection("teams");
+            // Connect to the database
+            const  mongodb = app.currentUser.mongoClient("mongodb-atlas");
+            const  collection = mongodb.db("Score-estimation-core").collection("teams");
 
-                // Watch for changes in the 'teams' collection
-                for await (const change of collection.watch()) {
-                    const activeDev = change.fullDocument?.developers.filter(
-                        (developer) => developer.status === "ACTIVE"
-                    );
-                    setActiveDevelopers(activeDev || []);
-                }
-            } catch (error) {
-                console.error("Error:", error);
+            // Everytime a change happens in the stream, add it to the list of events
+            for  await (const  change  of  collection.watch()) {
+                setEvents(events  => [...events, change]);
             }
-        };
+        }
         login();
     }, []);
 
-    return (
-        <div className="App">
-            {!!user && (
-                <div className="App-header">
-                    <h1>Connected as user {user.id}</h1>
+  // Return the JSX that will generate HTML for the page
+
+  return (
+        <div  className="App">
+            {!!user &&
+                <div  className="App-header">
+                    <h1>Connected as user ${user.id}</h1>
                     <div>
-                        <p>Active Developers:</p>
-                        <ul>
-                            {activeDevelopers.map((developer, index) => (
-                                <li key={index}>{developer.name}</li>
+                        <p>Latest events:</p>
+                        <table>
+                            <thead>
+                            <tr><td>Operation</td><td>Document Key</td><td>Full Document</td></tr>
+                            </thead>
+                            <tbody>
+                            {events.map((e, i) => (
+                                <tr  key={i}><td>{e.operationType}</td><td>{e.documentKey._id.toString()}</td><td>{JSON.stringify(e.fullDocument)}</td></tr>
                             ))}
-                        </ul>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-            )}
+            }
         </div>
-    );
+  );
 };
 
-export default App;
+export  default  App;
